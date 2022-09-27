7a7c19cd90d9cf1c5ad286ab886aa2cf7f159172
cheese <zoojj3319@gmail.com>
2022-09-27 15:23:12 +0900

    Refactor the file name and solve sum of intervals in 2D array

--- algorism-study/doitQ4.py
+++ algorism-study/doitQ4.py
@@ -1,17 +1,21 @@
+# 코드 출력으론 x,y 깔끔하게 시각화 된 상태로 나오지 않는다 헷갈ㄴㄴㄴ
+
 import sys
 input = sys.stdin.readline
-suNo, quizNo = map(int, input().split())
-numbers = list(map(int, input().split()))
-total_num = []
-temp = 0
+n, m = map(int, input().split())
+A = [[0] * (n+1)]
+D = [[0] * (n+1) for _ in range(n + 1)]
 
-for i in numbers:
-    temp = temp + i
-    total_num.append(temp)
+for i in range(n):
+    A_row = [0] + [int(x) for x in input().split()]
+    A.append(A_row)
 
-for i in range(quizNo):
-    start, end = map(int, input().split())
-    if start == 1:
-        print(total_num[end-1])
-    else:
-        print(total_num[end-1] - total_num[start-2])
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        # 합 배열 구하기
+        D[i][j] = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + A[i][j]
+
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    result = D[x2][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1-1][y1-1]
+    print(result)
